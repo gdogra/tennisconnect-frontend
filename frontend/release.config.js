76db407390aa0e438ca7582d1bf1a0f112340121
gautam dogra <gdogra@gmail.com>
2025-03-28 16:20:44 -0700

    chore: configure semantic-release for npm publishing

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -4,18 +4,16 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-    }],
-    ['@semantic-release/npm', {
-      npmPublish: true,
-      tarballDir: 'dist',
-    }],
-    ['@semantic-release/git', {
-      assets: ['CHANGELOG.md', 'package.json'],
-      message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
-    }],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
 
--- release.config.js
+++ release.config.js
@@ -1,18 +1,21 @@
-// release.config.js
 module.exports = {
-  branches: ["main"],
+  branches: ['main'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
   ],
 };
 
