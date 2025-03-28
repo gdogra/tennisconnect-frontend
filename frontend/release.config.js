e10efa250189d5ef5a6aeb822980eb5c18524954
gautam dogra <gdogra@gmail.com>
2025-03-28 16:54:26 -0700

    chore: remove npm publishing from semantic-release

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -1,19 +1,12 @@
 module.exports = {
   branches: ['main'],
-  tagFormat: 'v${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/git',
+    // '@semantic-release/npm', // removed because we're not publishing to npm
   ],
 };
 
