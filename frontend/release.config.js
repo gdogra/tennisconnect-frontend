d8ceabf89eb38332211c73a56b5b80fc051e2221
gautam dogra <gdogra@gmail.com>
2025-03-28 10:55:39 -0700

    chore: update auto-release workflow with correct permissions and config

--- frontend/release.config.js
+++ frontend/release.config.js
@@ -1,16 +1,18 @@
 module.exports = {
   branches: ["main"],
+  repositoryUrl: "https://github.com/gdogra/tennisconnect-frontend",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog", {
-      changelogFile: "CHANGELOG.md"
-    }],
-    ["@semantic-release/git", {
-      assets: ["CHANGELOG.md", "package.json"],
-      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    "@semantic-release/github"
-  ]
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
 };
 
